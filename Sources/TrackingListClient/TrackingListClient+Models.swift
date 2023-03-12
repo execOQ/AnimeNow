0a5269c69c9a2ba1c310697ffd06c72892b7d5c8
ErrorErrorError <16653389+ErrorErrorError@users.noreply.github.com>
2023-03-11 20:32:12 -0600

    style: run swiftformat

--- Sources/TrackingListClient/TrackingListClient+Models.swift
+++ Sources/TrackingListClient/TrackingListClient+Models.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 
+// MARK: - TrackingListClient.AuthState
+
 public extension TrackingListClient {
     enum AuthState: Equatable {
         case unauthenticated
@@ -20,6 +22,8 @@ public extension TrackingListClient {
     }
 }
 
+// MARK: - TrackableList
+
 public protocol TrackableList {
     associatedtype MediaID
     associatedtype User: Equatable
@@ -27,10 +31,13 @@ public protocol TrackableList {
     var name: String { get }
     var initialize: @Sendable () async throws -> Void { get }
     var logIn: @Sendable () async throws -> User { get }
-    var logOut: @Sendable () async -> Void { get }
-    var loggedIn: @Sendable () async -> Bool { get }
+    var logOut: @Sendable ()
+        async -> Void { get }
+    var loggedIn: @Sendable ()
+        async -> Bool { get }
     var refreshUser: @Sendable () async throws -> User { get }
-    var user: @Sendable () async -> User? { get }
+    var user: @Sendable ()
+        async -> User? { get }
     var userCollections: @Sendable () async throws -> Collections { get }
     var sync: @Sendable (MediaID, Int?) async throws -> Void { get }
 }
